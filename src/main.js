8fcaaac0aa0959725b2a95ae8eefce5d362606e7
Leon_Leeb <you.t5be@gmx.at>
2023-12-04 16:19:49 +0100

    render cube

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+/*
+const loader = new GLTFLoader();
+
+loader.load( './testCube.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+	alert("model not found");
+} );*/
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
