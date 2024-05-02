dbb0d9d50248b67fff588428410c9f0e88c16a7c
Leon_Leeb <you.t5be@gmx.at>
2024-05-02 16:59:27 +0200

    basic layout for a chat

--- src/client/util/FaceUtil.ts
+++ src/client/util/FaceUtil.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import html2canvas from 'html2canvas';
 
 export default class FaceUtil {
     private readonly scene = new THREE.Scene()
