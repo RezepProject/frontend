7f79c09b963280c12fc0dbd38599f27ba3be95f4
Leon_Leeb <you.t5be@gmx.at>
2024-05-16 10:03:40 +0200

    hub connection

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,6 +3,34 @@ import FaceUtil from "./util/FaceUtil";
 import { startWebSocket } from './util/SpeechToTextUtil'
 import { QuestionHandler } from './questionHandler/QuestionHandler'
 
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { TokenUtil } from './util/TokenUtil'
+
+const connection = new HubConnectionBuilder()
+    .withUrl("http://localhost:5260/hubs/connection", {
+        accessTokenFactory: async () => (await TokenUtil.getInstance()).getToken() + ""
+    })
+    //.withAutomaticReconnect()
+    .build();
+
+connection.on("connected", (connectionId: string) => {
+    console.log("Connected with ID:", connectionId);
+});
+
+connection.on("disconnected", (error: Error | undefined) => {
+    if (error) {
+        console.error("Disconnected:", error);
+    } else {
+        console.log("Disconnected");
+    }
+});
+
+connection.start().then(() => {
+    console.log("Connection established!");
+}).catch(err => {
+    console.error("Connection error:", err);
+});
+
 document.addEventListener("DOMContentLoaded", async () => {
     // TODO: Comment in the following line to start the WebSocket server
     //await startWebSocket();
